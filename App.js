7006df513da7db1ce44f3f72732f971ea0440cbe
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-05 02:48:43 -0500

    Complete Fundamental Concepts

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, SafeAreaView, Alert, Button, StatusBar , Platform} from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button 
+      color={"black"}
+      title='Click Me'
+        onPress={() => Alert.prompt("My Title", "My Message", text => console.log(text))}/>
+    </SafeAreaView>
   );
 }
+
+const containerStyle = {backgroundColor: "orange"};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'red',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
   },
 });
