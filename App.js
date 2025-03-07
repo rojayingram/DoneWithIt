b6922bd09130725525497e42f6232e9326bec01c
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-07 07:58:20 -0500

    Flexbox-flexBasis, flexGrow and flexShrink

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 // import { StatusBar } from 'expo-status-bar';
 import {
-  View
+  View,
 } from 'react-native';
 
 
 
 
 export default function App() {
-
   return (
     <View
     style={{
@@ -17,46 +16,24 @@ export default function App() {
       flexDirection: "row",
       justifyContent: "center",
       alignItems: "center",
-      alignContent: "center",
-      flexWrap: "wrap",
     }}>
-
-      <View
-      style={{
+      <View style={{
         backgroundColor: "dodgerblue",
+        // flexBasis: 100, // width or height
+        // flexGrow: 1, // === flex: 1
+        // flexShrink: 1, // === flex: -1
         width: 100,
-        height: 300,
-      }}/>
+        height: 100}}/>
 
-      <View
-      style={{
+      <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 100
-      }}/>
+        height: 100}}/>
 
-      <View
-      style={{
+      <View style={{
         backgroundColor: "tomato",
         width: 100,
-        height: 100
-      }}/>
-
-      <View
-      style={{
-        backgroundColor: "grey",
-        width: 100,
-        height: 100
-      }}/>
-
-      <View
-      style={{
-        backgroundColor: "green",
-        width: 100,
-        height: 100
-      }}/>
-
-
+        height: 100}}/>
     </View>
   );
 }
