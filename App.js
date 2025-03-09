5014c336edad62fdbc2bdd3d4890e70951503fcc
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-08 21:23:41 -0500

    11-Building the Welcome Screen

--- App.js
+++ App.js
@@ -1,58 +1,18 @@
 import React from 'react';
 // import { StatusBar } from 'expo-status-bar';
 import {
-  Image,
-  ImageBackground,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  ImageBase,
+  View
 } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
  
-const image = {uri: 'https://legacy.reactjs.org/logo-og.png'}
+// const image = {uri: 'https://legacy.reactjs.org/logo-og.png'}
 
 
 export default function App() {
   return (
-    <>
-    <View style={{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginTop: 40,
-      backgroundColor: 'black'
-    }}>
-    <View style={{
-      width: 50,
-      height: 50,
-      backgroundColor: '#fc5c65',
-      marginLeft:20,
-      
-    }}/>
-
-    <View style={{
-      width: 50,
-      height: 50,
-      backgroundColor: '#4ECDC4',
-      marginRight: 20,
-    }}/>
-    </View>
-    <View>
-      <Image source={require('./assets/chair.jpg')}
-      style={{
-        width: 570,
-        height: 570,
-        resizeMode: 'contain',
-        alignSelf:'center',
-        marginTop: 100
-      }}/>
-    </View>
-    </>
+    <WelcomeScreen/>
   );
 }
 
- const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
- });
+ // rnss
+ //rsf
