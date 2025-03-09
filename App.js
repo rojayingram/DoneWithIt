9a890b75f5c715e8916fe87212f1c359addadf40
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-08 21:53:56 -0500

    12-Building the View Image

--- App.js
+++ App.js
@@ -4,14 +4,13 @@ import {
   View
 } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
  
 // const image = {uri: 'https://legacy.reactjs.org/logo-og.png'}
 
 
 export default function App() {
-  return (
-    <WelcomeScreen/>
-  );
+  return (<ViewImageScreen/>);
 }
 
  // rnss
