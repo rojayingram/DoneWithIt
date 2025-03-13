f0c8f5855405a770293feabb5bbed89ec2218d40
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-12 22:51:59 -0500

    Styling -5- Styling Text

--- App.js
+++ App.js
@@ -1,41 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
  
 
 export default function App() {
   return (
-    <View style={{
+    <View 
+    style={{
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: "center",
+      alignItems: "center",
     }}>
-      <View style={{
-        width: 100,
-        height: 100,
-        padding: 20,
-        paddingLeft: 30,
-        paddingVertical: 10,
-        paddingHorizontal: 10,
-        backgroundColor: "dodgerblue",
-      }}>
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 50,
-            height: 50,
-          }}
-        />
-      </View>
-      <View
-      style={{
-        width: 100,
-        margin: 20,
-        height: 100,
-        backgroundColor: "tomato",
-      }}/>
+    <Text
+    style={{
+      fontSize: 30,
+      lineHeight: 30,
+      color: "tomato",
+      fontWeight: "bold",
+      fontStyle: "italic",
+      textAlign: "center",
+      fontFamily: "roboto", // {"courier" = iOS & android} {"roboto" = android}
+      textTransform: "capitalize",
+      textDecorationLine: "underline",
+    }}>
+      I love React Native! This is my first React native app! Here's some more text.
+    </Text>
     </View>
   );
 }
-
