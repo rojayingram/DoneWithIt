338ffcb2cea3f9a521f2aa3226a6105c1b5df8d7
rojay ingram <rojayingramcodes@gmail.com>
2025-03-20 23:34:48 -0500

    Styling -14- Building the Listing Details Screen

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+
+function ListingDetailsScreen(props) {
+    return (
+        <View>
+            <Image style={styles.image} source={require('../assets/jacket.jpg')}/>
+            <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>Red Jacket for sale</AppText>
+            <AppText style={styles.price}>$100</AppText>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    detailsContainer: {
+        padding: 20,
+    },
+
+    image: {
+        width: '100%',
+        height: 300,
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "500",
+    },
+
+    price: {
+        color: colors.secondary,
+        fontSize: 20,
+        fontWeight: "bold",
+        marginVertical: 10,
+    }
+})
+export default ListingDetailsScreen;
