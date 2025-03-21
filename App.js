3a58af845e8ca736e7a39e88534bdc97d52d0188
rojay ingram <rojayingramcodes@gmail.com>
2025-03-21 08:27:20 -0500

    Styling -16- Improving the View Image Screen

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import AppButton from './app/components/AppButton';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
