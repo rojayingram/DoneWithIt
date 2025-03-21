1385bc755114081b2192cf21c96270c0728df81f
rojay ingram <rojayingramcodes@gmail.com>
2025-03-21 00:22:39 -0500

    Styling -15- Building the ListItem Component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -3,6 +3,7 @@ import {Image, StyleSheet, View} from 'react-native';
 
 import AppText from '../components/AppText';
 import colors from '../config/colors';
+import ListItem from '../components/ListItem';
 
 function ListingDetailsScreen(props) {
     return (
@@ -11,6 +12,15 @@ function ListingDetailsScreen(props) {
             <View style={styles.detailsContainer}>
             <AppText style={styles.title}>Red Jacket for sale</AppText>
             <AppText style={styles.price}>$100</AppText>
+
+            <View style={styles.userContainer}>
+                <ListItem
+                    image={require("../assets/rojay.png")}
+                    title="Rojay Ingram"
+                    subTitle="5 Listings"
+                />
+            </View>
+            
             </View>
         </View>
     );
@@ -26,16 +36,21 @@ const styles = StyleSheet.create({
         height: 300,
     },
 
-    title: {
-        fontSize: 24,
-        fontWeight: "500",
-    },
-
     price: {
         color: colors.secondary,
         fontSize: 20,
         fontWeight: "bold",
         marginVertical: 10,
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "500",
+    },
+
+    userContainer: {
+        marginVertical: 40,
     }
+
 })
 export default ListingDetailsScreen;
