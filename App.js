4aba8fb66fa6d81f3e38ccc9aed06b61659a2cc9
rojay ingram <rojayingramcodes@gmail.com>
2025-03-25 19:41:58 -0500

    Lists -11- Building the Icon Component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
+
 import AppButton from './app/components/AppButton';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white"
+        />
+    </Screen>
   );
 }
