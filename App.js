b89adfc0b100cab98f41e750b0f81152540de1ab
rojay ingram <rojayingramcodes@gmail.com>
2025-04-03 19:49:04 -0500

    Form -2- Buidling the Login Form

--- App.js
+++ App.js
@@ -15,23 +15,10 @@ import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
+import LoginScreen from './app/screens/LoginScreen';
 
-const categories = [
-  { label: "Furniture",  value: 1 },
-  { label: "Clothing",  value: 2 },
-  { label: "Cameras",  value: 3 },
-];
 
 export default function App() {
-  const [ category, setCategory ] = useState(categories[0]);
 
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}  icon="apps" placeholder="Category"/>
-      <AppTextInput icon="email" placeholder="Email"/>
-    </Screen>
-  );
+  return <LoginScreen />
 }
