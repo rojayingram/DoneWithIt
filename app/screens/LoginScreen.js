2bd26f243174855bbf844fced3f4de87e2678a8c
rojay ingram <rojayingramcodes@gmail.com>
2025-04-17 09:09:42 -0500

    Form -9- Building the Form Component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -4,6 +4,7 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
+import AppForm from '../components/AppForm';
 import AppFormField from '../components/AppFormField';
 import SubmitButton from '../components/SubmitButton';
 
@@ -21,38 +22,33 @@ function LoginScreen(props) {
                 style={styles.logo}
                 source={require("../assets/logo-red.png")} />
                 
-                <Formik
+                <AppForm
                     initialValues={{email: '', password: ''}}
                     onSubmit={values => console.log(values)}
                     validationSchema={validationSchema}
                 >
-                    {() => (
-                        <>
-                            <AppFormField
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                icon="email"
-                                keyboardType="email-address"
-                                name={"email"}
-                                placeholder="Email"
-                                textContentType="emailAddress"
-                            />
+                    <AppFormField
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        icon="email"
+                        keyboardType="email-address"
+                        name={"email"}
+                        placeholder="Email"
+                        textContentType="emailAddress"
+                     />
                             
-                            <AppFormField
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                icon="lock"
-                                name={"password"}
-                                placeholder="Password"
-                                secureTextEntry
-                                textContentType="password"
-                            />
+                    <AppFormField
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        icon="lock"
+                        name={"password"}
+                        placeholder="Password"
+                        secureTextEntry
+                        textContentType="password"
+                    />
 
-                            <SubmitButton title={"Login"}/>
-                            
-                        </>
-                    )}
-                </Formik>
+                        <SubmitButton title={"Login"}/>
+                </AppForm>
                 
                 
         </Screen>
